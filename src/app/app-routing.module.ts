fb62f64ae16f41efcc97a9b4eb5bae6aebb5fc3c
jason <jason@jasons-MacBook-Pro.local>
2019-09-16 14:35:41 +0800

    updated with information page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,8 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  }
+  },
+  { path: 'information', loadChildren: './pages/information/information.module#InformationPageModule' }
 ];
 @NgModule({
   imports: [
