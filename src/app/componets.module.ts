af1bf88b5b9213532ffdc8adc9ab63a00bfdf8c8
GetALittleRough <260928310@qq.com>
2019-09-17 11:37:47 +0800

    import shared components bug fixed

--- src/app/componets.module.ts
+++ src/app/componets.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core'
+import { IonicModule } from '@ionic/angular';
 import { MajorsComponent } from './components/majors/majors.component';
 
 @NgModule({
+  imports: [IonicModule],
   declarations: [MajorsComponent],
   exports: [MajorsComponent]
 })
